49e83d5852d4a4750929802b9ec9ca7d11b5d0c7
Fes <example.email@example>
2024-11-12 10:39:53 +0200

    updated battle.py - added show_hp_bar method and continued battle logic
    updated enemy.py - added total_hp instance attribute

--- game/battle.py
+++ game/battle.py
@@ -4,13 +4,27 @@ class Battle:
     def __init__(self, player, enemy):
         self.player = player
         self.enemy = enemy
-        self.menu = (f'      THE BATTLE HAS STARTED\n'
-                f'    YOU ARE FIGHTING AGAINST THE FEARSOME {self.enemy.name}\n'
+        self.menu = (f'    YOU ARE FIGHTING AGAINST THE FEARSOME {self.enemy.name}\n'
                      f'\n To use an ability press:\n'
                      f'{self.player.show_abilities()}')
 
+    def show_hp_bar(self, bar_length, total_hp, current_hp):
+        pass
+
+
+
     def start(self):
+        print('THE BATTLE HAS STARTED !')
         while self.player.current_hp > 0 and self.enemy.current_hp > 0:
             print(self.menu)
-            break
+
+            choice = int(input('> '))
+            if choice in range(1, len(self.player.abilities) + 1):
+                if choice == 1:
+                    if self.player.abilities[0].type == 'damage':
+                        self.enemy.current_hp -= self.player.abilities[0].amount
+
+            print(self.enemy.current_hp)
+
+
 
--- game/enemy.py
+++ game/enemy.py
@@ -3,18 +3,20 @@ from game.ability import GoblinAttack, Slash, Maim
 
 
 class Enemy(ABC):
-    def __init__(self, name, current_hp, abilities):
+    def __init__(self, name, total_hp):
         self.name = name
-        self.current_hp = current_hp
+        self.total_hp = total_hp
+        self.current_hp = total_hp
         self.abilities = []
+        self.xp_for_player = 0
 
 
 class Goblin(Enemy):
     def __init__(self):
-        super().__init__('Goblin', 50, [])
+        super().__init__('Goblin', 250)
+        self.xp_for_player = 100
         normal_attack, slash, maim = GoblinAttack(), Slash(), Maim()
         self.abilities.extend([normal_attack, slash, maim])
-
 
     def attack(self):
         if self.abilities[0].amount:
